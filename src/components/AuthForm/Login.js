6c847dab9ebeef2c4d4a25bad0c2a2adcbc668c8
Bilal <bsajid173@gmail.com>
2022-06-17 21:52:44 +0530

    Format login page

--- src/components/AuthForm/Login.js
+++ src/components/AuthForm/Login.js
@@ -1,6 +1,6 @@
 import { useRef, Fragment } from "react";
 import classes from "./Login.module.css";
-import image from "../../images/login.jpg"
+import image from "../../images/login.jpg";
 
 const LoginForm = (props) => {
   const emailInputRef = useRef();
@@ -13,38 +13,37 @@ const LoginForm = (props) => {
     console.log(enteredEmail, enteredPassword);
   };
   return (
-    <Fragment>   
-     <div className={classes.maincontainer}>            
-      <div className={classes.logincontainer}>
-        <form onSubmit={formSubmitHandler}>
-          <div className={classes.username}>
-            <input
-            placeholder="Username"
-              type="email"
-              id="email"
-              autoComplete="off"
-              required
-              ref={emailInputRef}
-            />
-          </div>
-          <div className={classes.password}>
-            <input
-            placeholder="Password"
-              type="password"
-              id="password"
-              required
-              ref={passwordInputRef}
-            />
-          </div>
+    <Fragment>
+      <div className={classes.maincontainer}>
+        <div className={classes.logincontainer}>
+          <form onSubmit={formSubmitHandler}>
+            <div className={classes.username}>
+              <input
+                placeholder="Username"
+                type="email"
+                id="email"
+                autoComplete="off"
+                required
+                ref={emailInputRef}
+              />
+            </div>
+            <div className={classes.password}>
+              <input
+                placeholder="Password"
+                type="password"
+                id="password"
+                required
+                ref={passwordInputRef}
+              />
+            </div>
             <button type="submit">Login</button>
             <h4>New here? SignUp</h4>
-        </form>
+          </form>
+        </div>
+        <div className={classes.loginimg}>
+          <img src={image} alt="img" />
+        </div>
       </div>
-      <div className={classes.loginimg}>
-        <img src={image} alt="img" />
-      </div>
-     </div>      
-      
     </Fragment>
   );
 };
