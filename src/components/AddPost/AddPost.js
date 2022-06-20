b4adf9f41b23709f51385674c21fa22774b4df80
Bilal <bsajid173@gmail.com>
2022-06-20 15:02:20 +0530

    Add validation to addpost form

--- src/components/AddPost/AddPost.js
+++ src/components/AddPost/AddPost.js
@@ -2,17 +2,57 @@ import classes from "./AddPost.module.css";
 import useInput from "../../hooks/use-input";
 
 const AddPost = () => {
+  const {
+    value: enteredTitle,
+    hasError: titleHasError,
+    valueChangeHandler: titleChangeHandler,
+    onBlurHandler: titleBlurHandler,
+    isValid: titleIsValid,
+    reset: resetTitle,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: enteredContent,
+    hasError: contentHasError,
+    valueChangeHandler: contentChangeHandler,
+    onBlurHandler: contentBlurHandler,
+    isValid: contentIsValid,
+    reset: resetContent,
+  } = useInput((value) => value.trim() !== "");
+
+  const titleClasses = titleHasError ? classes.invalid : "";
+  const contentClasses = contentHasError ? classes.invalid : "";
   return (
     <div className={classes.addPostWrapper}>
       <div className={classes.addpostform}>
         <form>
-          <div>
+          <div className={titleClasses}>
             <label htmlFor="title">Title</label>
-            <input id="title" name="title" type="text" />
+            {titleHasError && (
+              <p className={classes.error}>Please Enter a title.</p>
+            )}
+            <input
+              id="title"
+              name="title"
+              type="text"
+              value={enteredTitle}
+              onChange={titleChangeHandler}
+              onBlur={titleBlurHandler}
+            />
           </div>
-          <div>
+          <div className={contentClasses}>
             <label htmlFor="content">What do you wanna say?</label>
-            <textarea rows="8" name="content" id="content"></textarea>
+            {contentHasError && (
+              <p className={classes.error}>Please Enter some content.</p>
+            )}
+            <textarea
+              rows="8"
+              name="content"
+              id="content"
+              value={enteredContent}
+              onChange={contentChangeHandler}
+              onBlur={contentBlurHandler}
+            ></textarea>
           </div>
           <div className={classes.actions}>
             <button type="submit">Add Post</button>
