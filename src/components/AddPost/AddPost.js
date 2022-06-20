c9bdca5262d32f49ea2de87917a4c9083462faaa
Bilal <bsajid173@gmail.com>
2022-06-20 14:41:02 +0530

    Add post form styled

--- src/components/AddPost/AddPost.js
+++ src/components/AddPost/AddPost.js
@@ -1,7 +1,27 @@
 import classes from "./AddPost.module.css";
+import useInput from "../../hooks/use-input";
 
 const AddPost = () => {
-  return;
+  return (
+    <div className={classes.addPostWrapper}>
+      <div className={classes.addpostform}>
+        <form>
+          <div>
+            <label htmlFor="title">Title</label>
+            <input id="title" name="title" type="text" />
+          </div>
+          <div>
+            <label htmlFor="content">What do you wanna say?</label>
+            <textarea rows="8" name="content" id="content"></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button type="submit">Add Post</button>
+            <button>Cancel</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddPost;
