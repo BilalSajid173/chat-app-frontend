798d9726316c3e783b82b62c2ea9c313f6339fad
Nasrul <nasrul2001@gmail.com>
2022-06-22 11:01:00 +0530

    Designed All posts page

--- src/components/AllPosts/PostItem.js
+++ src/components/AllPosts/PostItem.js
@@ -3,31 +3,33 @@ import classes from "./PostItem.module.css";
 const PostItem = (props) => {
     return (
         <>
-    <div className={classes.postContainer}>
-        <div className={classes.singlepost}>
-            <h3>{props.author}</h3>
-            <p>{props.content}</p>
-            <p>{props.createdAt}</p>
-        </div>
-        <div className={classes.actions}>
-            <div className={classes.like}>
-                <button>
-                    <span class="material-symbols-outlined">
-                        favorite
-                    </span>
-                        Like
-                </button>
+        <div className={classes.postbg}>
+            <div className={classes.postContainer}>
+                <div className={classes.singlepost}>
+                    <h3>{props.author}</h3>
+                    <p>{props.content}</p>
+                    <p>{props.createdAt}</p>
+                </div>
+                <div className={classes.actions}>
+                    <div className={classes.like}>
+                        <button>
+                            <span class="material-symbols-outlined">
+                                favorite
+                            </span>
+                                Like
+                        </button>
+                    </div>
+                    <div className={classes.comment}>
+                        <button>
+                            <span class="material-symbols-outlined">
+                                comment
+                            </span>
+                                Comment
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className={classes.comment}>
-                <button>
-                    <span class="material-symbols-outlined">
-                        comment
-                    </span>
-                        Comment
-                </button>
-            </div>
         </div>
-    </div>
         </>
     );
 };
--- src/components/AuthForm/Login.js
+++ src/components/AuthForm/Login.js
@@ -70,49 +70,51 @@ const LoginForm = (props) => {
   const passwordClasses = passwordHasError ? classes.invalid : "";
   return (
     <Fragment>
-      <div className={classes.maincontainer}>
-        <div className={classes.logincontainer}>
-          <h2>Sign In</h2>
-          <form onSubmit={formSubmitHandler} noValidate>
-            <div className={emailClasses}>
-              {emailHasError && (
-                <p className={classes.error}>Please Enter a valid email.</p>
-              )}
-              <input
-                placeholder="Username"
-                type="email"
-                id="email"
-                autoComplete="off"
-                required
-                value={enteredEmail}
-                onChange={emailChangeHandler}
-                onBlur={emailBlurHandler}
-              />
-            </div>
-            <div className={passwordClasses}>
-              {passwordHasError && (
-                <p className={classes.error}>
-                  Min passsword length is 7 characters.
-                </p>
-              )}
-              <input
-                placeholder="Password"
-                type="password"
-                id="password"
-                required
-                value={enteredPassword}
-                onChange={passwordChangeHandler}
-                onBlur={passwordBlurHandler}
-              />
-            </div>
-            <button type="submit">Sign In</button>
-            <h4>
-              Not a member? <Link to="/signup">Sign Up</Link>
-            </h4>
-          </form>
-        </div>
-        <div className={classes.loginimg}>
-          <img src={image} alt="img" />
+      <div className={classes.bodygradient}>
+        <div className={classes.maincontainer}>
+          <div className={classes.logincontainer}>
+            <h2>Sign In</h2>
+            <form onSubmit={formSubmitHandler} noValidate>
+              <div className={emailClasses}>
+                {emailHasError && (
+                  <p className={classes.error}>Please Enter a valid email.</p>
+                )}
+                <input
+                  placeholder="Username"
+                  type="email"
+                  id="email"
+                  autoComplete="off"
+                  required
+                  value={enteredEmail}
+                  onChange={emailChangeHandler}
+                  onBlur={emailBlurHandler}
+                />
+              </div>
+              <div className={passwordClasses}>
+                {passwordHasError && (
+                  <p className={classes.error}>
+                    Min passsword length is 7 characters.
+                  </p>
+                )}
+                <input
+                  placeholder="Password"
+                  type="password"
+                  id="password"
+                  required
+                  value={enteredPassword}
+                  onChange={passwordChangeHandler}
+                  onBlur={passwordBlurHandler}
+                />
+              </div>
+              <button type="submit">Sign In</button>
+              <h4>
+                Not a member? <Link to="/signup">Sign Up</Link>
+              </h4>
+            </form>
+          </div>
+          <div className={classes.loginimg}>
+            <img src={image} alt="img" />
+          </div>
         </div>
       </div>
     </Fragment>
