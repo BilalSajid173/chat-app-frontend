97481334d3bde3bdd7697198342da1bb24af21ac
Bilal <bsajid173@gmail.com>
2022-07-02 16:24:45 +0530

    User page responsive

--- src/components/UserAccount/UserAccount.js
+++ src/components/UserAccount/UserAccount.js
@@ -87,6 +87,54 @@ const UserAccount = () => {
               </p>
             </div>
           </div>
+          <div className={classes.userimg_mobile}>
+            <img src={userimg} alt="img"></img>
+          </div>
+          <div className={classes.userinfo_mobile}>
+            <div className={classes.nameaddress}>
+              <h1>{user.name}</h1>
+              {user.address && <h4>{user.address}</h4>}
+              <p>bsajid173@gmail.com</p>
+              {user.number && <p>{user.number}</p>}
+              <div className={classes.socials}>
+                {user.linkedin && (
+                  <a rel="noreferrer" target="_blank" href={user.linkedin}>
+                    <i className="fa-brands fa-linkedin"></i>
+                  </a>
+                )}
+                {user.github && (
+                  <a rel="noreferrer" target="_blank" href={user.github}>
+                    <i className="fa-brands fa-github"></i>
+                  </a>
+                )}
+              </div>
+            </div>
+            <div className={classes.actions}>
+              <Link to="/friendlist">
+                <i className="fa-solid fa-user-group"></i>
+              </Link>
+              <button>
+                <i className="fa-solid fa-pen-to-square"></i>
+              </button>
+              <Link to="">
+                <i className="fa-solid fa-bookmark"></i>
+              </Link>
+            </div>
+          </div>
+          <div className={classes.mobile_bio}>
+            <h4>Bio</h4>
+            <p>
+              Nec ullamcorper sit amet risus nullam eget felis. Integer vitae
+              justo eget magna fermentum iaculis eu non diam. Laoreet sit amet
+              cursus sit amet. Blandit turpis cursus in hac habitasse. Nec
+              ullamcorper sit amet risus nullam eget felis. Integer vitae justo
+              eget magna fermentum iaculis eu non diam. Laoreet sit amet cursus
+              sit amet. Blandit turpis cursus in hac habitasse. Nec ullamcorper
+              sit amet risus nullam eget felis. Integer vitae justo eget magna
+              fermentum iaculis eu non diam. Laoreet sit amet cursus sit amet.
+              Blandit turpis cursus in hac habitasse.
+            </p>
+          </div>
           <div className={classes.userposts}>
             <h2>Your Posts</h2>
             {allPosts.map((post) => (
